33a6886d35803900834d2141d847d356f6ddc444
logiph <sosend2009@gmail.com>
2016-08-10 18:26:50 +0800

    添加OpenGL的view，和依赖的framework

---
+++ HelloOpenGL/HelloOpenGL/OpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLView.h
+//  HelloOpenGL
+//
+//  Created by logiph on 8/10/16.
+//  Copyright © 2016 logiph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLView : UIView
+
+@end
---
+++ HelloOpenGL/HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by logiph on 8/10/16.
+//  Copyright © 2016 logiph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
