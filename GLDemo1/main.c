45424aebd53c0f09f6ccc5c19fbaf31a19c867b5
logiph <logiph@debian.home>
2016-08-16 04:53:58 -0400

    add triangle

--- GLDemo1/main.c
+++ GLDemo1/main.c
@@ -19,9 +19,34 @@ void lineSegment()
     glColor3f(0.0, 0.4, 0.2);
 
     glBegin(GL_LINES);
-        glVertex2i(180, 15);
-        glVertex2i(10, 145);
+        glVertex2i(15, 15);
+        glVertex2i(30, 145);
     glEnd();
+
+    glBegin(GL_POINTS);
+        glVertex2f(50, 15);
+        glVertex2f(50, 16);
+        glVertex2f(50, 17);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(100, 30);
+        glVertex2f(100, 90);
+        glVertex2f(160, 30);
+    glEnd();
+
+
+    glBegin(GL_TRIANGLE_STRIP);
+        //glRecti(60, 15, 90, 30);
+        glVertex2i(60, 15);
+        glVertex2i(90, 15);
+        glVertex2i(80, 20);
+        glVertex2i(70, 50);
+        glVertex2i(60, 60);
+    glEnd();
+
+
+
     glFlush();
 }
 
